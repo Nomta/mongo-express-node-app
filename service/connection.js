5da831cc5dd648244eea9d71ee150b6aeb03eb96
Nomta <nomta@mail.ru>
2021-01-16 21:06:58 +0300

    db connection setup

--- service/connection.js
+++ service/connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 // const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const config = require('../config');
 
-// mongoose.set('useNewUrlParser', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 // mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
 
 // mongoose.set('debug', false);
 
